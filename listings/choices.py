caace9a3d4bc06b8932abd1c135fc1ea1d71d8e8
jeving <85601392+jeving@users.noreply.github.com>
2021-06-12 10:11:29 +0530

    Add files via upload

---
+++ listings/choices.py
@@ -0,0 +1,45 @@
+bedroom_choices = {
+    '1':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+    '10':10,
+    '11':11,
+    '12':12,
+}
+
+price_choices = {
+    '20000000':'₹20,000,000',
+    '30000000':'₹30,000,000',
+    '40000000':'₹40,000,000',
+    '50000000':'₹50,000,000',
+    '60000000':'₹60,000,000',
+    '70000000':'₹70,000,000',
+    '80000000':'₹80,000,000',
+    '90000000':'₹90,000,000',
+    '1000000000':'₹1,000,000,000',
+    '10000000000':'₹1M+',
+}
+
+state_choices = {
+    'Sindh': 'Sindh',
+    'Balochistan': 'Balochistan',
+    'Khyber Pakhtunkha': 'Khyber Pakhtunkha',
+    'Punjab': 'Punjab',
+    'Gilgit Baltistan': 'Gilgit Baltistan',
+    'Maharastra': 'Maharastra',
+}
+
+city_choices = {
+    'MUM':'Mumbai',
+    'PU' :'pune',
+    'NA' :'nagpur',
+    'NS' :'nashik',
+    'TH' :'thane',
+}
+
